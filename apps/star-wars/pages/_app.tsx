616c1a4bfc17c43704f2eb6923c81996affb9336
Tejas <tejasnamjoshi9@gmail.com>
2021-01-24 18:35:24 +0530

    Initial commit

--- apps/star-wars/pages/_app.tsx
+++ apps/star-wars/pages/_app.tsx
@@ -9,12 +9,10 @@ function CustomApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Welcome to star-wars!</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossOrigin="anonymous"></link>
       </Head>
       <div className="app">
-        <header className="flex">
-          <NxLogo width="75" height="50" />
-          <h1>Welcome to star-wars!</h1>
-        </header>
+        
         <main>
           <Component {...pageProps} />
         </main>
